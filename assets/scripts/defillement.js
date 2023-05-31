f09ce2346fba7703be953ee5911f139cd4e8409f
Ana Dumont <dev.anais.dumont@gmail.com>
2023-05-31 15:01:04 +0200

    Gestion de la nav

--- assets/scripts/defillement.js
+++ assets/scripts/defillement.js
@@ -3,29 +3,40 @@ $(document).ready(function () {
   let currentSection = 0;
   let isScrolling = false;
 
-  // Variables pour gérer l'animation du header et du footer
+  // Variables pour gérer l'animation du header, du footer, et de la nav
   let header = $("header");
   let footer = $("footer");
+  let nav = $("nav");
 
-  // Fonction pour animer le déplacement du header et du footer hors de l'écran
+  // Fonction pour animer le déplacement du header, du footer, et de la nav hors de l'écran
   function animateElementsOut() {
-    header.addClass("header-hidden"); // Ajoute la classe pour masquer le header
-    footer.addClass("footer-hidden"); // Ajoute la classe pour masquer le footer
+    header.addClass("fade");
+    footer.addClass("fade");
+    nav.addClass("fade");
   }
 
-  // Fonction pour animer le retour du header et du footer sur l'écran
+  // Fonction pour animer le retour du header, du footer, et de la nav sur l'écran
   function animateElementsIn() {
-    header.removeClass("header-hidden"); // Supprime la classe pour afficher le header
-    footer.removeClass("footer-hidden"); // Supprime la classe pour afficher le footer
+    header.removeClass("fade");
+    footer.removeClass("fade");
+    nav.removeClass("fade");
 
     // Gestion de la couleur d'affichage en fonction de la parité de la section
     if (currentSection % 2 === 1) {
       header.css("color", "var(--bleu-clair)");
       footer.css("color", "var(--bleu-clair)");
+      $("img").attr("src", "./assets/images/logos/logo-bleu.png");
+      nav.css("color", "var(--bleu-clair)");
     } else {
       header.css("color", "var(--violet-clair)");
       footer.css("color", "var(--violet-clair)");
+      $("img").attr("src", "./assets/images/logos/logo-violet.png");
+      nav.css("color", "var(--violet-clair)");
     }
+
+    // Ajouter la classe active à l'icône correspondante à la section actuelle
+    nav.find("i").removeClass("active");
+    nav.find("i").eq(currentSection).addClass("active");
   }
 
   // Fonction pour faire défiler les sections
@@ -52,6 +63,12 @@ $(document).ready(function () {
       }, 1000);
     }
   }
+
+  // Gestion du clic sur les icônes de la navigation
+  nav.find("i").on("click", function () {
+    let index = $(this).index();
+    scrollToSection(index);
+  });
 
   $(window).on("wheel", function (event) {
     let delta = event.originalEvent.deltaY;
