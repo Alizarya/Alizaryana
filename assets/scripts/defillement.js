f8787ed47643b08ac02301422ecbe8ddaec18901
Ana Dumont <dev.anais.dumont@gmail.com>
2023-05-31 10:30:53 +0200

    Ajout du footer et du changement de couleur

--- assets/scripts/defillement.js
+++ assets/scripts/defillement.js
@@ -3,25 +3,28 @@ $(document).ready(function () {
   let currentSection = 0;
   let isScrolling = false;
 
-  // Variables pour gérer l'animation du header
+  // Variables pour gérer l'animation du header et du footer
   let header = $("header");
-  let headerWidth = header.outerWidth();
-  let windowWidth = $(window).width();
+  let footer = $("footer");
 
-  // Fonction pour animer le déplacement du header hors de l'écran
-  function animateHeaderOut() {
+  // Fonction pour animer le déplacement du header et du footer hors de l'écran
+  function animateElementsOut() {
     header.addClass("header-hidden"); // Ajoute la classe pour masquer le header
+    footer.addClass("footer-hidden"); // Ajoute la classe pour masquer le footer
   }
 
-  // Fonction pour animer le retour du header sur l'écran
-  function animateHeaderIn() {
+  // Fonction pour animer le retour du header et du footer sur l'écran
+  function animateElementsIn() {
     header.removeClass("header-hidden"); // Supprime la classe pour afficher le header
+    footer.removeClass("footer-hidden"); // Supprime la classe pour afficher le footer
 
     // Gestion de la couleur d'affichage en fonction de la parité de la section
     if (currentSection % 2 === 1) {
       header.css("color", "var(--bleu-clair)");
+      footer.css("color", "var(--bleu-clair)");
     } else {
       header.css("color", "var(--violet-clair)");
+      footer.css("color", "var(--violet-clair)");
     }
   }
 
@@ -40,12 +43,12 @@ $(document).ready(function () {
       );
       currentSection = index;
 
-      // Animation du header lors du défilement
-      animateHeaderOut();
+      // Animation du header et du footer lors du défilement
+      animateElementsOut();
 
-      // Réapparition du header après 1 seconde
+      // Réapparition du header et du footer après 1 seconde
       setTimeout(function () {
-        animateHeaderIn();
+        animateElementsIn();
       }, 1000);
     }
   }
