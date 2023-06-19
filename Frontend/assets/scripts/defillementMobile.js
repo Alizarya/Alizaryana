87ad9ee053c575dc735d408a64014a338a11f059
Ana Dumont <dev.anais.dumont@gmail.com>
2023-06-19 17:02:34 +0200

    Nav responsive ok

--- Frontend/assets/scripts/defillementMobile.js
+++ Frontend/assets/scripts/defillementMobile.js
@@ -9,6 +9,24 @@ $(document).ready(function () {
       // Afficher la section correspondante en fonction de l'index
       $("section").addClass("hidden");
       $("section").eq(index).removeClass("hidden");
+
+      // Mettre à jour les couleurs de la barre de navigation
+      updateNavColors(index);
     });
+
+    function updateNavColors(index) {
+      let nav = $("nav");
+      if (index % 2 === 1) {
+        nav.css("color", "var(--bleu-clair)");
+        nav.css("background-color", "var(--bleu-fonce)");
+      } else {
+        nav.css("color", "var(--violet-clair)");
+        nav.css("background-color", "var(--violet-fonce)");
+      }
+    }
+
+    // Initialiser les couleurs de la barre de navigation avec la section actuellement affichée
+    let currentIndex = $("section:not(.hidden)").index();
+    updateNavColors(currentIndex);
   }
 });
